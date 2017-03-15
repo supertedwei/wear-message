e827c01e36949e492e20b3517b23d7c667edaa1c
Ted Wei <jdwei@hotmail.com>
2017-03-15 14:38:04 +0800

    display Data on wearable device

--- wear/src/main/java/com/supergigi/wearmessage/WearMainActivity.java
+++ wear/src/main/java/com/supergigi/wearmessage/WearMainActivity.java
@@ -16,12 +16,9 @@ import java.util.Locale;
 
 public class WearMainActivity extends WearableActivity {
 
-    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
-            new SimpleDateFormat("HH:mm", Locale.US);
-
     private BoxInsetLayout mContainerView;
-    private TextView mTextView;
-    private TextView mClockView;
+    private TextView text1View;
+    private TextView text2View;
 
     public static final void startActivity(Context context, MessageData messageData) {
         Intent intent = new Intent(context, WearMainActivity.class);
@@ -34,42 +31,16 @@ public class WearMainActivity extends WearableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setAmbientEnabled();
 
-        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
-        mClockView = (TextView) findViewById(R.id.clock);
-    }
+        text1View = (TextView) findViewById(R.id.text1);
+        text2View = (TextView) findViewById(R.id.text2);
 
-    @Override
-    public void onEnterAmbient(Bundle ambientDetails) {
-        super.onEnterAmbient(ambientDetails);
-        updateDisplay();
-    }
-
-    @Override
-    public void onUpdateAmbient() {
-        super.onUpdateAmbient();
-        updateDisplay();
-    }
-
-    @Override
-    public void onExitAmbient() {
-        updateDisplay();
-        super.onExitAmbient();
-    }
-
-    private void updateDisplay() {
-        if (isAmbient()) {
-            mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
-            mTextView.setTextColor(getResources().getColor(android.R.color.white));
-            mClockView.setVisibility(View.VISIBLE);
-
-            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
-        } else {
-            mContainerView.setBackground(null);
-            mTextView.setTextColor(getResources().getColor(android.R.color.black));
-            mClockView.setVisibility(View.GONE);
+        MessageData messageData = (MessageData) getIntent().getSerializableExtra("MessageData");
+        if (messageData != null) {
+            text1View.setText(messageData.getText1());
+            text2View.setText(messageData.getText2());
         }
+
     }
+
 }
